327068088d73fd6419aa977b3c61bc69b22c4d20
IshinoJun <t.mac.jun.0303@gmail.com>
2019-04-28 15:40:54 +0900

    make it better

--- server/index.js
+++ server/index.js
@@ -30,17 +30,13 @@ async function start() {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   });
 
   // Give nuxt middleware to express
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-    app.use('/api', apiRouter)
-    app.use(nuxt.render)
-  });
+  app.use('/api', apiRouter)
+  app.use(nuxt.render)
 
   // Listen the server
   app.listen(port, host)
